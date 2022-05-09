62aa0b814d0e0f6a3d1a1df018c5933490c435e3
Maksim Burtsev <nim.int3@gmail.com>
2022-05-09 18:18:01 +0300

    Benchmarks

---
+++ Benchmark/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<WriteBench>();
+
+            Console.WriteLine("Press any key for exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Benchmark/WriteBench.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkDotNet.Attributes;
+using DataflowChannel;
+
+namespace Benchmark
+{
+    [Config(typeof(BenchConfig))]
+    public class WriteBench
+    {
+        private const int count = 100_000_000;
+        private Channel<int> _channel;
+
+        [IterationSetup(Target = nameof(Write0))]
+        public void Write0Setup()
+        {
+            _channel = new Channel<int>();
+        }
+
+        [Benchmark(OperationsPerInvoke = count)]
+        public void Write0()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _channel.Write0(1);
+            }
+        }
+
+        [IterationSetup(Target = nameof(Write1))]
+        public void Write1Setup()
+        {
+            _channel = new Channel<int>();
+        }
+
+        [Benchmark(OperationsPerInvoke = count)]
+        public void Write1()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _channel.Write1(1);
+            }
+        }
+
+        [IterationSetup(Target = nameof(Write2))]
+        public void Write2Setup()
+        {
+            _channel = new Channel<int>();
+        }
+
+        [Benchmark(OperationsPerInvoke = count)]
+        public void Write2()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _channel.Write2(1);
+            }
+        }
+    }
+}
